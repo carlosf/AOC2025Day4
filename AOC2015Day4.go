2290a25e231309ade8030bf8820df173573025c0
Carlos Ferreira <carlosf@northrock.bm>
2024-01-03 23:55:36 -0400

    Isolated the first 5 characters

--- AOC2015Day4.go
+++ AOC2015Day4.go
@@ -17,12 +17,21 @@ func main() {
 	for {
 		postfixNum++
 		hash := md5.Sum(append([]byte(input), []byte(strconv.FormatInt(postfixNum, 10))...))
-		p(string(postfixNum))
+		fmt.Printf("current number is %v\n", postfixNum)
+		fmt.Printf("length of hash is %d\n", len(hash))
+		// for statement for length of hash.
 
-		var sumFirstFiveChars = hash[:5]
-		fmt.Printf("sumFirstFiveChars is %x\n", sumFirstFiveChars[0])
+		var sumFirstFiveChars string = ""
 
-		if false {
+		for _, v := range hash {
+			sumFirstFiveChars += fmt.Sprintf("%x", v)
+			p(sumFirstFiveChars)
+		}
+		p(sumFirstFiveChars[:5])
+
+		fmt.Printf("sumFirstFiveChars is %d\n", sumFirstFiveChars[0])
+
+		if true {
 			fmt.Printf("hash 0 is %x\n", hash[0])
 			fmt.Printf("hash 1 is %x\n", hash[1])
 			fmt.Printf("hash 2 is %x\n", hash[2])
